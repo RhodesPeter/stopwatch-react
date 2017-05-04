b34176ce1345b377cf27cac92fdd7bc9aff2a307
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-05-04 01:33:53 +0100

    Dynamically create new stopwatch onClick

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,25 @@ import Stopwatch from './Stopwatch.js';
 
 class App extends Component {
 
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      watches: [<Stopwatch key={0}/>],
+      count: 1
+    }
+  }
+
+  createNewStopwatch = () => {
+    this.setState({ count: this.state.count + 1 });
+    this.state.watches.push( <Stopwatch key={ this.state.count }/> );
+    return this.state.watches;
+  }
+
   render() {
     return (
       <div className="app">
-        <button>New Stopwatch</button>
-        <Stopwatch />
+        <button onClick={ this.createNewStopwatch }>+</button>
+        {  this.state.watches }
       </div>
     );
   }
