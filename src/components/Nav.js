9d53bc1e1c7f70ef3bf0ae0b08eec0bc8a614a91
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-05-08 00:56:01 +0100

    Add title to home page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,22 +4,14 @@ import { Link } from 'react-router-dom'
 class Nav extends Component {
 
   render() {
-    let navVersion = '';
-    if (this.props.refs === 'home'){
-      navVersion = <div className="nav__button" onClick={ this.props.onClick }>
-      Add Stopwatch <span className="nav__plus">+</span></div>
-    } else {
-      navVersion = <Link to='/'><div className="nav__button" onClick={ this.props.onClick }>
-      Add Stopwatch <span className="nav__plus">+</span></div></Link>
-    }
-
     return (
       <div className="nav">
         <div className="nav__title"><Link to='/'>Stopwatch</Link></div>
         <div className="nav__spacer">&#183;</div>
         <div className="nav__info"><Link to='/info'>Info</Link></div>
         <div className="nav__spacer-two">&#183;</div>
-        { navVersion }
+        <Link to='/'><div className="nav__button" onClick={ this.props.onClick }>
+        Add Stopwatch<span className="nav__plus">+</span></div></Link>
       </div>
     );
   }
